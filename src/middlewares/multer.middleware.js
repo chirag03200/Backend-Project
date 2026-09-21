e61c86e269b6160ca007a00a587807281a83b1a2
chirag03200 <jainc3620@gmail.com>
2026-09-21 23:23:45 +0530

    Routes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
         cb(null, file.originalname);
     }
-});
+}); 
 
 export const upload = multer({ 
     storage,
